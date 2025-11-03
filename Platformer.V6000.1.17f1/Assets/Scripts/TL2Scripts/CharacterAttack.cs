5394bef6c8cd512495e0e76c03150b47cb3d501b
GabrielBybee <gabriel.m.bybee@gmail.com>
2025-11-03 02:34:21 -0800

    Directional Hitbox and Boss Damage
    Added a Directional hitbox that moves where the player is facing, fixed the animation for attacking, and made the boss able to take damage from player clicking(only unarmed for now).

--- Platformer.V6000.1.17f1/Assets/Scripts/TL2Scripts/CharacterAttack.cs
+++ Platformer.V6000.1.17f1/Assets/Scripts/TL2Scripts/CharacterAttack.cs
@@ -1,16 +1,44 @@
+using System.Collections;
+using Codice.Client.Common.GameUI;
 using UnityEngine;
 
 public class CharacterAttack : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private CharacterMove clickDetection;
+    [SerializeField] private CapsuleCollider2D weaponHitbox;
+    [SerializeField] private Animator animator;
+    [SerializeField] private BossAI_Advanced bossScript;
+    [SerializeField] private CapsuleCollider2D bossHitbox;
+    private int typeAttack = 0;
+    
+    
+
+    public IEnumerator Attack()
     {
+        animator.SetBool("IsAttacking", true);
+        if (weaponHitbox.IsTouching(bossHitbox))
+        {
+            if (animator.GetBool("HasWeapon1") == true)
+            {
+                typeAttack = 1;
+            }
+
+            switch (typeAttack)
+            {
+                case 1:
+                    bossScript.TakeDamage(10);
+                    break;
+
+                default:
+                    bossScript.TakeDamage(5);
+                    Debug.Log("ATTACKED BOSS");
+                    break;
+            }
+        }
         
+        yield return new WaitForSeconds(0.3f);
+        animator.SetBool("IsAttacking", false);
+
     }
 }
